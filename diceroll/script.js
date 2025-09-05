68cb655c4fb666e5fe78f6e174bba8045dcf0476
palmanish-coder <manishpal222125@gmail.com>
2025-09-05 11:46:23 +0530

    first update

--- diceroll/script.js
+++ diceroll/script.js
@@ -1,26 +1,53 @@
-const diceImage = document.getElementById("diceImage");
-const rollHistory = document.getElementById("rollHistory");
+const buttonEl = document.getElementById("rollBtn");
+const dice1El = document.getElementById("dice1");
+const dice2El = document.getElementById("dice2");
+const rollHistoryEl = document.getElementById("roll-history");
+const winnerMsgEl = document.getElementById("winner-msg");
+const score1El = document.getElementById("score1");
+const score2El = document.getElementById("score2");
+// Dice face array
+const diceFace = ["&#9856;", "&#9857;", "&#9858;", "&#9859;", "&#9860;", "&#9861;"];
 
-function rollDice() {
-  // Add spinning effect
-  diceImage.classList.add("spin");
+let score1 = 0;
+let score2 = 0;
 
-  let counter = 0;
-  const interval = setInterval(() => {
-    const tempRoll = Math.floor(Math.random() * 6) + 1;
-    diceImage.src = `dice${tempRoll}.png`;
-    counter++;
-  }, 150);
+buttonEl.addEventListener("click", () => {
+   
+    const random1 = Math.floor(Math.random() * 6);
+    const random2 = Math.floor(Math.random() * 6);
 
-  setTimeout(() => {
-    clearInterval(interval);
-    diceImage.classList.remove("spin");
+    const currentFace1 = diceFace[random1];
+    const currentFace2 = diceFace[random2];
 
-    const roll = Math.floor(Math.random() * 6) + 1;
-    diceImage.src = `dice${roll}.png`;
+    //animation reset krna
+    dice1El.classList.remove("roll-animation");
+    dice2El.classList.remove("roll-animation");
+    void dice1El.offsetWidth;
+    void dice2El.offsetWidth;
+    dice1El.classList.add("roll-animation");
+    dice2El.classList.add("roll-animation");
 
-    const listItem = document.createElement("li");
-    listItem.textContent = `You rolled: ${roll}`;
-    rollHistory.prepend(listItem);
-  }, 2000); // roll time = 2 seconds
-}
+    //Dice faces update
+    dice1El.innerHTML = currentFace1;
+    dice2El.innerHTML = currentFace2;
+
+    
+    if (random1 > random2) {
+        winnerMsgEl.textContent = "🏆 Player 1 Wins!";
+        score1++;
+    } else if (random2 > random1) {
+        winnerMsgEl.textContent = "🏆 Player 2 Wins!";
+        score2++;
+    } else {
+        winnerMsgEl.textContent = "👑 It's a Draw!";
+    }
+
+    
+    score1El.textContent = score1;
+    score2El.textContent = score2;
+
+   //history update
+    const historyItem = document.createElement("li");
+    historyItem.innerHTML = `Player 1: ${random1 + 1} <span>${currentFace1}</span> | Player 2: ${random2 + 1} <span>${currentFace2}</span>`;
+    rollHistoryEl.prepend(historyItem);
+});
